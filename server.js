c58d904feaff3f426cc5d8a3d310101dfb8f9e3b
sandeep4102 <sandeep.sarswat@mail.vinove.com>
2020-09-26 16:04:30 +0530

    changes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/developInnovaPolymers'));
+app.use(express.static(__dirname + '/dist/shareapp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/developInnovaPolymers/index.html'));
+res.sendFile(path.join(__dirname+'/dist/shareapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
